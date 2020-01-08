c69b2d6f0ce8b3f1202efcddf4258a2c73c441fb
anhongyu1 <anhongyu@jd.com>
2020-01-08 17:27:38 +0800

    improve test_rvo

--- test_rvo.cpp
+++ test_rvo.cpp
@@ -13,6 +13,10 @@ public:
         std::cout << "copy constructor" << std::endl;
         this->val_ = obj.val_;
     }
+    TestObject(TestObject&& obj) {
+        std::cout << "move constructor" << std::endl;
+        this->val_ = obj.val_;
+    }
     TestObject& operator=(const TestObject& rhs) {
         std::cout << "copy assignment" << std::endl;
         this->val_ = rhs.val_;
@@ -22,6 +26,9 @@ public:
         std::cout << "move assignment" << std::endl;
         this->val_ = rhs.val_;
         return *this;
+    }
+    ~TestObject() {
+        std::cout << "TestObject " << val_ << " deleted." << std::endl;
     }
 
     void setValue(int val) {val_ = val;}
@@ -37,9 +44,28 @@ TestObject getTestObject() {
     return obj;
 }
 
+TestObject getTestObjectViaMove() {
+    TestObject obj;
+    obj.setValue(13);
+    return std::move(obj);
+}
+
+}
+
+static void test_rvo1() {
+    rvo::TestObject obj1 = rvo::getTestObject();
+    std::cout << obj1.getValue() << std::endl;
+}
+
+static void test_rvo2() {
+    rvo::TestObject obj2 = rvo::getTestObjectViaMove();
+    std::cout << obj2.getValue() << std::endl;
 }
 
 void test_rvo() {
-    rvo::TestObject obj = rvo::getTestObject();
-    std::cout << obj.getValue() << std::endl;
+    test_rvo1();
+
+    std::cout << "======================" << std::endl;
+
+    test_rvo2();
 }
